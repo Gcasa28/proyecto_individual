7f039f8a49c39ddb732f7ba9bd9bf8e30040ed48
Gcasa28 <gabrielcasanov8@gmail.com>
2023-05-15 17:51:21 -0400

    prueba14

--- main.py
+++ main.py
@@ -10,58 +10,58 @@ app = FastAPI()
 def peliculas_mes(mes):
     '''Determina la cantidad de películas que se estrenaron en el mes dado'''
    
-    respuesta = df['id'][df['release_month'] == mes].count()
-    return {'mes': mes, 'cantidad': int(respuesta)}
+    peliculas_mes = df['id'][df['release_month'] == mes].count()
+    return {'mes': mes, 'cantidad': int(peliculas_mes)}
 
 @app.get("/peliculas_dia/{dia}")
 def peliculas_dia(dia):
     '''Determina la cantidad de películas que se estrenaron en el día dado'''
    
-    respuesta = df['id'][df['release_day'] == dia].count()
-    return {'dia': dia, 'cantidad': int(respuesta)}
+    peliculas_dia = df['id'][df['release_day'] == dia].count()
+    return {'dia': dia, 'cantidad': int(peliculas_dia)}
 
 @app.get("/franquicia/{franquicia}")
 def franquicia(franquicia):
     '''Determina la cantidad de películas que pertenece a la franquicia dada, la ganancia total y la ganancia promedio'''
    
-    respuesta1 = df['id'][df['belongs_to_collection'] == franquicia].count()
-    respuesta2 = np.sum(df['return'][df['belongs_to_collection'] == franquicia])
-    respuesta3 = np.mean(df['return'][df['belongs_to_collection'] == franquicia])
-    return {'franquicia': franquicia, 'cantidad': int(respuesta1), 'ganancia_total': respuesta2, 'ganancia_promedio': respuesta3}
+    cantidad_peliculas = df['id'][df['belongs_to_collection'] == franquicia].count()
+    ganacia_total = np.sum(df['return'][df['belongs_to_collection'] == franquicia])
+    ganacia_promedio = np.mean(df['return'][df['belongs_to_collection'] == franquicia])
+    return {'franquicia': franquicia, 'cantidad': int(cantidad_peliculas), 'ganancia_total': ganacia_total, 'ganancia_promedio': ganacia_promedio}
 
 @app.get("/peliculas_pais/{pais}")
 def peliculas_pais(pais):
     '''Determina la cantidad de películas producidas en el país dado'''
 
-    respuesta = 0
+    cantidad_peliculas = 0
     n = df['production_countries'].size
     for i in range(n):
         if pais in df.iloc[i, 8]:
-            respuesta += 1
-    return {'pais': pais, 'cantidad': respuesta}
+            cantidad_peliculas += 1
+    return {'pais': pais, 'cantidad': cantidad_peliculas}
 
 @app.get("/productoras/{productora}")
 def productoras(productora):
     '''Determina la ganancia total y la cantidad de películas de la productora dada'''
 
-    respuesta1 = 0
-    respuesta2 = 0
+    ganancia_total = 0
+    cantidad_peliculas = 0
     n = df['production_companies'].size
     for i in range(n):
         if productora in df.iloc[i, 7]:
-            respuesta1 += df.iloc[i, 20]
-            respuesta2 += 1
-    return {'productora': productora, 'ganancia_total': respuesta1, 'cantidad': respuesta2}
+            ganancia_total += df.iloc[i, 20]
+            cantidad_peliculas += 1
+    return {'productora': productora, 'ganancia_total': ganancia_total, 'cantidad': cantidad_peliculas}
 
 
 @app.get("/pelicula/{pelicula}")
 def retorno(pelicula):
     '''Determina la inversion, la ganancia, el retorno de inversión y el año en el que se lanzo la pelicula dada'''
 
-    respuesta1 = 0
-    respuesta2 = 0
-    respuesta3 = 0
-    respuesta4 = 0
+    inversion = 0
+    ganancia = 0
+    retorno = 0
+    anio = 0
     n = df['title'].size
     for i in range(n):
         if df.iloc[i, 15] == pelicula:
@@ -69,4 +69,4 @@ def retorno(pelicula):
             respuesta2 += df.iloc[i, 10]
             respuesta3 += df.iloc[i, 20]
             respuesta4 = df.iloc[i, 17]
-    return {'pelicula':pelicula, 'inversion':respuesta1, 'ganacia':respuesta2,'retorno':respuesta3, 'anio':int(respuesta4)}
+    return {'pelicula':pelicula, 'inversion':inversion, 'ganacia':ganancia,'retorno':retorno, 'anio':int(anio)}
